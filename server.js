6081c7cf3fe6dad35c0cc06827973c2183cea267
Ali Ahmad <alisibby786@gmail.com>
2021-08-03 14:16:03 +0530

    first push

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-const port=process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
-app.get("/",(req,res)=>{
-    res.send({message:"bhosdike"});
+app.get("/", (req, res) => {
+    res.send({ message: "Smaroh server is up and running" });
 });
 
 app.listen(port, function (err, serve) {
